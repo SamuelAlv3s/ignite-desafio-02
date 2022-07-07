7b9ba9d5dad110536a25ce2616d4e27845815ee2
Samuel <samuelalves681@gmail>
2022-07-07 00:16:31 -0300

    component header

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setSelectedGenre: (id: number) => void;
+}
+export function SideBar({
+  genres,
+  selectedGenreId,
+  setSelectedGenre,
+}: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => setSelectedGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
